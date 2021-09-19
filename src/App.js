4e71d4c3d1e21b37de2ea66b1f6bfcb28242d69b
Darley36 <darleybest@gmail.com>
2021-09-19 00:13:28 -0500

    StoreProvider created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,48 @@
+import React, {createContext, useContext, useReducer} from 'react';
 
+const HOST_API = "http://localhost:8080/api"
+
+const initialState={
+  list: []
+};
+const Store =createContext(initialState)
+
+const List = () =>{
+  const {dispatch, state} = useContext(Store)
+  return <div>
+    <table >
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Tarea</td>
+          <td>¿Completado?</td>
+        </tr>
+      </thead>
+      <tbody>
+        {currentList.map((todo) => {
+          return <tr key={todo.id} style={todo.completed ? decorationDone : {}}>
+            <td>{todo.id}</td>
+            <td>{todo.name}</td>
+            <td><input type="checkbox" defaultChecked={todo.completed} onChange={(event) => onChange(event, todo)}></input></td>
+            <td><button onClick={() => onDelete(todo.id)}>Eliminar</button></td>
+            <td><button onClick={() => onEdit(todo)}>Editar</button></td>
+          </tr>
+        })}
+      </tbody>
+    </table>
+  </div>
+}
+
+
+
+const StoreProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return <Store.Provider value={{ state, dispatch }}>
+    {children}
+  </Store.Provider>
+
+}
 
 function App() {
   return (
